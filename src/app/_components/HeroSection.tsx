454c79aa91ba3031305a90cdc62dbaa8add0f759
Silas Matos <silas.progamacao@gmail.com>
2025-03-24 17:50:46 -0300

    .

--- src/app/_components/HeroSection.tsx
+++ src/app/_components/HeroSection.tsx
@@ -1,32 +1,40 @@
 'use client'
+
 import React, { useRef } from 'react'
 import VariableProximity from '../../components/VariableProximity/VariableProximity'
 import StarBorder from '../../components/StarBorder/StarBorder'
 
 const HeroSection: React.FC = () => {
   const containerRef = useRef<HTMLDivElement | null>(null)
+
   return (
-    <section className="hero-section text-white py-20 px-6">
+    <section className="hero-section text-white py-20 px-6" ref={containerRef}>
       <div className="container mx-auto">
-        <span className="text-2xl md:text-4xl font-bold mb-6 text-accent">
-          &lt;&gt;
-        </span>
         <h1 className="text-4xl md:text-6xl font-bold mb-6">
-          Desenvolvedor <br />
-          <span className="">Full Stack</span>
+          <VariableProximity
+            label="Desenvolvedor Full Stack"
+            fromFontVariationSettings="'wght' 100"
+            toFontVariationSettings="'wght' 900"
+            containerRef={containerRef}
+            radius={150}
+            falloff="linear"
+            className="inline-block"
+          />
         </h1>
-        <span className="text-2xl md:text-4xl font-bold mb-6 text-accent">
-          &lt;/&gt;
-        </span>
-        <VariableProximity
-          label="Ao longo dos anos, participei de diversos projetos robustos, tanto na parte de back-end quanto front-end, o que me permitiu adquirir uma visão abrangente e aprimorar minhas habilidades técnicas. Sempre fui motivado pela resolução de problemas."
-          fromFontVariationSettings="'wght' 300"
-          toFontVariationSettings="'wght' 700"
-          containerRef={containerRef}
-          radius={100}
-          falloff="gaussian"
-          className="inline-block"
-        />
+        <p className="text-lg md:text-xl leading-relaxed mb-6">
+          <VariableProximity
+            label="Ao longo dos anos, participei de diversos projetos robustos, tanto na parte de back-end quanto front-end, o que me permitiu adquirir uma visão abrangente e aprimorar minhas habilidades técnicas. Sempre fui motivado pela resolução de problemas."
+            fromFontVariationSettings="'wght' 300"
+            toFontVariationSettings="'wght' 700"
+            containerRef={containerRef}
+            radius={100}
+            falloff="gaussian"
+            className="inline-block"
+          />
+        </p>
+        <StarBorder as="a" href="#contact" className="mt-4">
+          Curriculo
+        </StarBorder>
       </div>
     </section>
   )
