e2cdbd09c62292e46b63eb0d9f849c8665beb01d
Silas <silas.progamacao@gmail.com>
2025-03-25 00:33:57 -0300

    img adicionanda

--- src/app/_components/HeroSection.tsx
+++ src/app/_components/HeroSection.tsx
@@ -6,13 +6,15 @@ import StarBorder from '../../components/StarBorder/StarBorder'
 import Button from '../../components/ui/Button'
 import Particles from '../../components/Particles/Particles' // Import the Particles component
 import Orb from '@/components/Orb/Orb'
+import hero_image from '@/assets/hero-image.png'
+import Image from 'next/image'
 
 const HeroSection: React.FC = () => {
   const containerRef = useRef<HTMLDivElement | null>(null)
 
   return (
     <section
-      className="hero-section h-full min-h-screen overflow-hidden flex items-center justify-center relative text-white py-20 px-6"
+      className="hero-section h-full min-h-screen overflow-hidden flex items-center justify-center container mx-auto relative text-white py-20 px-6"
       ref={containerRef}
     >
       <div className="absolute inset-0 -z-10">
@@ -33,10 +35,10 @@ const HeroSection: React.FC = () => {
 
       <div className="container mx-auto relative z-10 flex items-center ">
         <div>
-          <h1 className="text-4xl md:text-6xl font-bold mb-6">
+          <h1 className="text-3xl md:text-6xl font-bold mb-6">
             Desenvolvedor Full Stack
           </h1>
-          <p className="text-xl md:text-xl leading-relaxed mb-6">
+          <p className="text-lg md:text-xl leading-relaxed mb-6">
             <VariableProximity
               label="Ao longo dos anos, participei de diversos projetos robustos, tanto na parte de back-end quanto front-end, o que me permitiu adquirir uma visão abrangente e aprimorar minhas habilidades técnicas. Sempre fui motivado pela resolução de problemas."
               fromFontVariationSettings="'wght' 300"
@@ -50,7 +52,7 @@ const HeroSection: React.FC = () => {
           <Button text="">Curriculo</Button>
         </div>
         <div>
-          <Orb />
+          <Image src={hero_image} alt="Hero Image" className="" />
         </div>
       </div>
     </section>
